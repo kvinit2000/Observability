45c187550608d3e1d9078c14c2e544cb153eace6
kvinit2000 <41991980+kvinit2000@users.noreply.github.com>
2025-11-04 21:07:01 +0000

    updated

--- src/main/java/com/observability/config/MeterRegistryConfig.java
+++ src/main/java/com/observability/config/MeterRegistryConfig.java
@@ -1,10 +1,9 @@
 package com.observability.config;
 
+import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
-import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
-import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
-import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,5 +15,16 @@ public class MeterRegistryConfig {
     public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
         return registry -> registry.config().commonTags("application", "ObservabilityDemo");
     }
-}
 
+    // This is REQUIRED for @Timed to work
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
+    }
+
+    // This is REQUIRED for @Observed to work
+    @Bean
+    public ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+        return new ObservedAspect(observationRegistry);
+    }
+}
