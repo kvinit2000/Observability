88c5b9a06437c21e7971f304ba75eac912e39947
kvinit2000 <41991980+kvinit2000@users.noreply.github.com>
2025-11-04 20:34:35 +0000

    updated

--- src/main/java/com/observability/ObservabilityApplication.java
+++ src/main/java/com/observability/ObservabilityApplication.java
@@ -2,8 +2,12 @@ package com.observability;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@Configuration
+@EnableAspectJAutoProxy
 public class ObservabilityApplication {
     public static void main(String[] args) {
         SpringApplication.run(ObservabilityApplication.class, args);
