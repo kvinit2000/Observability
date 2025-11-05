f3ed850fd92af25a161e7f463fcff6ca84620a26
kvinit2000 <41991980+kvinit2000@users.noreply.github.com>
2025-11-05 11:31:29 +0000

    updated

--- module-oltp/src/main/java/com/myoltp/SimpleApp.java
+++ module-oltp/src/main/java/com/myoltp/SimpleApp.java
@@ -2,15 +2,19 @@ package com.myoltp;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import java.util.UUID;
 
 public class SimpleApp {
     private static final Logger log = LoggerFactory.getLogger(SimpleApp.class);
     public static void main(String[] args) {
+        MDC.put("requestId", UUID.randomUUID().toString());
         log.info("App starting...");
 
         int result = computeSquare(5);
 
         log.info("Result = " + result);
+        MDC.clear();
     }
 
     // one method
